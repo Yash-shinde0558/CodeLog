8485878a2f97c800b4a7130acad5b35e7de99421
Your Name <yashshinde0558@gmail.com>
2026-05-30 19:31:59 +0530

    Refactor passingClassToMethods: Replace BankAccount implementation with car class for showroom demonstration

--- passingClassToMethods.java
+++ passingClassToMethods.java
@@ -1,53 +1,66 @@
 
-
-
-
-// Deposit money
-// Withdraw money
-// Check their balance
-
+// Problem
+// A car showroom wants to store:
+// Car brand
+// Car model
+// Car price
+// The car should be able to:
+// Start
+// Stop
+// Display details    
 public class passingClassToMethods {
-    
-    public static class BankAccount{
 
-    //Data Members
-    String Accountholder;
-    int Accountnumber;
-    double Balance;
+        public static class car{
+        
+            //Data Members 
+            String carBrand;
+            String model;
+            double price;
 
-    //Deposit Money
-    void deposit(double amount) {
-        Balance = Balance + amount;
-        System.out.println(amount + "deposited");
-    }
+            void start(){
+                System.out.println(model + " is started... ");
+            }
 
-    //Withdraw Money
-    void withdraw(double amount){
-        if(amount<=Balance){
-            Balance = Balance - amount;
-            System.out.println(amount + "Withdraw");
-        }else{
-            System.out.println("Insufficient Balance");
+            void stop(){
+                System.out.println(model + " is stop... ");
+            }
+
+            void carDetails(){
+                System.out.println("Car Brand" + carBrand);
+                System.out.println(model);
+                System.out.println(price);
+            }
         }
-    }
 
-    //Display Account Balance 
+    public static void main(String[] args){
 
-    void displayBalance(){
-        System.out.println("Account Holder: " + Accountholder);
-        System.out.println("Current Balance: " + Balance);
-    }
-}
-    public static void main(String[]args) {
+        //Object-1
+        car c1 = new car();
 
-        BankAccount  a1 = new BankAccount();
+        c1.carBrand = "BMW";
+        c1.model = "BMW M5";
+        c1.price = 20000000;
 
-        a1.Accountholder = "Yash Shinde";
-        a1.Accountnumber = 123456789;
-        a1.Balance = 10000;
+        //Object-2
+        car c2 = new car();
+        
+        c2.carBrand = "Hyundai";
+        c2.model = " i20 Active";
+        c2.price = 100000;
 
-        a1.deposit(1000);
-        a1.withdraw(2000);
-        a1.displayBalance();
+        // Using Methods 
+
+        //For object-1
+        c1.start();
+        c1.stop();
+        c1.carDetails();
+
+        //For object-2
+        c2.start();
+        c2.stop();
+        c2.carDetails();
+
+
+
     }
 }
