a0e2c9f5f19376f473487daefdd6dfe3fc467874
Your Name <yashshinde0558@gmail.com>
2026-06-05 20:43:37 +0530

    Implement PolyMorphism class with Dog, Cat, Pikachu, and Human subclasses, each providing a speak method

---
+++ PolyMorphism.java
@@ -0,0 +1,41 @@
+
+public class PolyMorphism {
+    
+    public static class Dog{
+        void speak(){
+            System.out.println("Bhaww Bhaww");
+        }
+    }
+
+    public static class Cat{
+        void speak(){
+            System.out.println("Maoww Maoww");
+        }
+    }
+
+    public static class Pikachu{
+        void speak(){
+            System.out.println("Pika Pika");
+        }
+    }
+
+    public static  class Human{
+        void speak(){
+            System.out.println("Hello");
+        }
+    }
+
+    public static void main(String[]args){
+        Dog d = new Dog();
+        Cat c = new Cat();
+        Pikachu p = new Pikachu();
+        Human h = new Human();
+
+
+        d.speak();
+        c.speak();
+        p.speak();
+        h.speak();
+
+    }
+}
