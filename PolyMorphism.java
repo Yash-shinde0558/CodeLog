620fec40b2c2a1eacc65982e1e4579cbda86daec
Your Name <yashshinde0558@gmail.com>
2026-06-10 23:04:21 +0530

    Refactor PolyMorphism class by removing unused subclasses and integrating Student class with function overloading methods

--- PolyMorphism.java
+++ PolyMorphism.java
@@ -1,41 +1,36 @@
 
-public class PolyMorphism {
-    
-    public static class Dog{
-        void speak(){
-            System.out.println("Bhaww Bhaww");
-        }
+
+
+    class Student{ //class
+    String name;
+    int age;
+    //Function overloading means same name function is create in one class 
+
+    //Functions return types should be different 
+    //If same return types then parameters should be differnt 
+
+    public void printInfo(String name){
+        System.out.println(name);
     }
 
-    public static class Cat{
-        void speak(){
-            System.out.println("Maoww Maoww");
-        }
+    public void printInfo(int age){
+        System.out.println(age);
     }
 
-    public static class Pikachu{
-        void speak(){
-            System.out.println("Pika Pika");
-        }
+    public void printInfo(String name , int age){
+        System.out.println(name + " " + age);
     }
+}
 
-    public static  class Human{
-        void speak(){
-            System.out.println("Hello");
-        }
-    }
-
+    public class PolyMorphism{ //File name
     public static void main(String[]args){
-        Dog d = new Dog();
-        Cat c = new Cat();
-        Pikachu p = new Pikachu();
-        Human h = new Human();
+        //Object1 
+        Student s1 = new Student();
+        s1.name = "Yash";
+        s1.age = 21;
 
-
-        d.speak();
-        c.speak();
-        p.speak();
-        h.speak();
+        s1.printInfo(s1.name,s1.age);
 
     }
+    
 }
