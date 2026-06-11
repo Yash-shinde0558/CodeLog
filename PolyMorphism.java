9a382d1ab4e00e8ea3ff9f10d52c965b66e4a48a
Your Name <yashshinde0558@gmail.com>
2026-06-11 19:52:30 +0530

    Refactor PolyMorphism class by removing Student class and its methods, retaining only the Calculator class with function overloading for addition

--- PolyMorphism.java
+++ PolyMorphism.java
@@ -1,36 +1,25 @@
 
+public class PolyMorphism{
 
+    public static class Calculator{
 
-    class Student{ //class
-    String name;
-    int age;
-    //Function overloading means same name function is create in one class 
+        int a;
+        int b;
+        int c;
 
-    //Functions return types should be different 
-    //If same return types then parameters should be differnt 
+        void add(int a, int b){
+            System.out.println("Sum= "+(a+b) );
+        }
 
-    public void printInfo(String name){
-        System.out.println(name);
+        void add(int a, int b , int c) { 
+            System.out.println("Sum= "+(a+b+c));
+        }
     }
 
-    public void printInfo(int age){
-        System.out.println(age);
+    public static void main(String[]args) {
+        Calculator c1 = new Calculator();
+
+        c1.add(10,20);
+        c1.add(10,20,30); 
     }
-
-    public void printInfo(String name , int age){
-        System.out.println(name + " " + age);
-    }
-}
-
-    public class PolyMorphism{ //File name
-    public static void main(String[]args){
-        //Object1 
-        Student s1 = new Student();
-        s1.name = "Yash";
-        s1.age = 21;
-
-        s1.printInfo(s1.name,s1.age);
-
-    }
-    
 }
