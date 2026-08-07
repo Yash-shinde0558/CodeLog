3894326092cf84de23856af7b8b01219962470de
Your Name <yashshinde0558@gmail.com>
2026-08-07 21:44:56 +0530

    Comment out example method calls in main for clarity

--- PolyMorphism.java
+++ PolyMorphism.java
@@ -21,7 +21,7 @@ public class PolyMorphism{
     public static void main(String[]args) {
         Calculator c1 = new Calculator();
 
-        c1.add(10,20);
-        c1.add(10,20,30,10); 
+        // c1.add(10,20);
+        // c1.add(10,20,30,10); 
     }
 }
