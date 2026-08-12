4b75992d660fbaa825cc73d051dedc4ebd8d7c71
Your Name <yashshinde0558@gmail.com>
2026-08-12 21:20:24 +0530

    Clarify comments in string class to enhance understanding of string comparison

--- string.java
+++ string.java
@@ -3,7 +3,11 @@ import java.util.Scanner;
 public class string {
     public static void main(String[] args) {
         
-        //Compare Strings
+        //Compare two string 
+        // String sentence = "TonyStark";
+        // String name = sentence.substring(0,4);
+        // System.out.println(name);
+
         String sentence = "TonyStark";
         String name = sentence.substring(0,4);
         System.out.println(name);
