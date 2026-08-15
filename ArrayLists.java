c106918a07e50aad6eeb65d2e503f1e64732c857
Your Name <yashshinde0558@gmail.com>
2026-08-15 13:03:41 +0530

    Refactor ArrayLists class to enhance element manipulation and add sorting functionality

--- ArrayLists.java
+++ ArrayLists.java
@@ -1,18 +1,35 @@
-
+import java.util.Collections;
 import java.util.ArrayList;
 public class ArrayLists {
     public static void main(String[]args) {
 
         ArrayList<Integer> list = new ArrayList<>();
 
-
         //Add Elements
         list.add(0);
-        list.add(1);
-        list.add(2);
         list.add(3);
+        list.add(2);
+        list.add(1);
         System.out.println(list);
 
+        // Get Elements
+        int element = list.get(3);
+        System.out.println(element);
+
+        //Add element between
+        list.add(1,1);
+        System.out.println(list);
+
+        int size = list.size();
+        System.out.println(size);
+
+        for(int i=0; i<list.size(); i++){
+            System.out.print(list.get(i));
+        }
+        System.out.println();
+
+        Collections.sort(list);
+        System.out.println(list);
 
     }
 }
